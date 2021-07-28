eb91d17762953ce9520ccb479144029b4c53ae89
Leo Cucinell <leocucinell@gmail.com>
2021-07-28 14:32:22 -0700

    return of el nombre

--- controllers/controller_1.js
+++ controllers/controller_1.js
@@ -12,5 +12,9 @@ router.get("/", (req, res, next) => {
     res.send("Hello form products index page!");
 });
 
+router.get("/elNombre", (req, res, next) => {
+    res.send("Just gonna watch and get credit doods");
+});
+
 /* SECTION: Export router */
 module.exports = router;
